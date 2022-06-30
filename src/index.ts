c8059fe89ffc04469f64370555d4e48e5eeb9ee1
muhammad gmal ezzat <59686905+muhammadgmalezzat@users.noreply.github.com>
2022-06-30 11:56:20 +0200

    comfigure base server and add jasmine configuration

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,15 @@
-console.log("fff");
+import express, { Application ,Request , Response} from 'express';
+
+
+const app:Application = express();
+
+const port = 3000;
+
+
+app.get('/', (req: Request, res: Response) => {
+    res.send("ffffffffffffffffffff");
+})
+
+app.listen(port, () => console.log(`listen on ${port}`));
+
+export default app;
---
+++ src/tests/indexSpec.ts
@@ -0,0 +1,14 @@
+import { Application, response } from "express";
+import supertest from "supertest";
+import app from "../index"
+
+
+const request = supertest(app);
+
+
+describe("test endpoint", () => {
+    it("get the / endpoint", async () => {
+        const response = await request.get('/');
+        expect(response.status).toBe(200);
+    })
+})
